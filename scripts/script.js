0477675256413316c50b23c8ed8b2dbdd2a2b3ce
zequ22 <eze22alderete@gmail.com>
2023-07-12 18:50:41 -0300

    Estructura HTML y CSS listas

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+var round = document.getElementById('rounds');
+var simonBtns = document.getElementsByClassName('btn');
+var startBtn = document.getElementById('btnStart');
+
+class Simon {
+    constructor(simonBtns, startBtn, round){
+        
+    }
+}
