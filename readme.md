0d3ea839b718bcc80eac7ef104e0b05844d5e9ee
zequ22 <eze22alderete@gmail.com>
2023-07-20 08:53:19 -0300

    Ajustes visuales

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 LPPA 2023 - Ingenieria en Sistemas Informaticos - UAI
 
 Alderete Ezequiel Alejandro
+
+https://zequ22.github.io/LPPA23_ALDERETE_SIMONDICE/
--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-var round = document.getElementById('rounds');
-var simonBtns = document.getElementsByClassName('btn');
-var startBtn = document.getElementById('btnStart');
-
-class Simon {
-    constructor(simonBtns, startBtn, round){
-        
-    }
-}
