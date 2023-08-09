58cbc7f0093d62dcfaff109a4d48bb696b2e3fab
zequ22 <eze22alderete@gmail.com>
2023-08-09 19:53:33 -0300

    Reordenamiento del codigo JS

--- scripts/game.js
+++ scripts/game.js
@@ -1,27 +1,29 @@
-// Declaración de variables
-var sequence = [];
-var lvl = 0;
-var colors = ['Red', 'Green', 'Blue', 'Yellow'];
-var btnColors = document.querySelectorAll('.deviceSimon > div');
-var btnStart = document.getElementById('btnPlay');
-var userSequence = [];
-var waitingAnswer = false;
-var playerScore = 0;
+'use strict'
 
 // Espera hasta que la página haya cargado completamente
 window.addEventListener('load', function () {
 
-  // Oculta los modals de contacto al cargar la página
+  // Declaración de variables
+  var sequence = [];
+  var lvl = 0;
+  var colors = ['Red', 'Green', 'Blue', 'Yellow'];
+  var btnColors = document.querySelectorAll('.deviceSimon > div');
+  var btnStart = document.getElementById('btnPlay');
+  var userSequence = [];
+  var waitingAnswer = false;
+  var playerScore = 0;
+
+  // Oculta el modal de contacto al cargar la página
   var modalContact = document.getElementById('modalContact');
   modalContact.style.display = 'none';
 
-  // Al hacer click en "Continuar" en el modal de nombre se oculta el modal y guardar el nombre del jugador
+  // Al hacer click en "Continuar" en el modal del Nombre, se oculta el modal y guardar el nombre del jugador
   var btnGetName = document.getElementById('btnCompleteName');
   btnGetName.addEventListener('click', hideModalName);
 
-  // Función para ocultar el modal de nombre y guardar el nombre del jugador
+  // Ocultar el modal de Nombre y guardar el nombre del jugador
   function hideModalName() {
-    var modalNames = document.getElementById('modalName');
+    var modalName = document.getElementById('modalName');
     var playerNameText = document.getElementById('namePlayer');
     var playerName = playerNameText.value;
   
@@ -30,78 +32,76 @@ window.addEventListener('load', function () {
       return;
     } else {
       var btnShowName = document.getElementById('btnName');
-      modalNames.style.display = 'none';
+      modalName.style.display = 'none';
       btnShowName.textContent = 'Jugador: ' + playerName;
     }
+  }
+    
+  // Muestra el modal de contacto 
+  var btnContact = document.getElementById('btnContact');
+  btnContact.addEventListener('click', showModalContact);
   
-    // Muestra el modal de contacto 
-    var btnContact = document.getElementById('btnContact');
-    btnContact.addEventListener('click', showModalContact);
-  
-    function showModalContact() {
-      modalContact.style.display = 'block';
-      btnStart.disabled=true;
-    }
-
-    // Al hacer click en "Cancelar" en el modal de contacto se oculta
-    var btnCancel = document.getElementById('btnCancel');
-    btnCancel.addEventListener('click', hideModalContact);
-
-    function hideModalContact() {
-      modalContact.style.display = 'none';
-      btnStart.disabled=false;
-    }
-
-    // Al hacer click en "Enviar" del formulario de contacto valida y envia el correo
-    var btnSendEmail = document.getElementById('btnSend');
-    btnSendEmail.addEventListener('click', sendEmail);
-
-    function sendEmail() {
-      var txtTransmitter = document.getElementById('emailContact');
-      var transmitter = txtTransmitter.value;
-      var txtName = document.getElementById('nameContact');
-      var name = txtName.value;
-      var txtMessage = document.getElementById('txtMessage');
-      var message = txtMessage.value;
-
-      var validateName = /^[a-zA-Z0-9\s]+$/;
-      if (!validateName.test(name)) {
-        alert('El nombre debe ser alfanumérico.');
-        return;
-      }
-
-      var validateEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!validateEmail.test(transmitter)) {
-        alert('El email ingresado es inválido.');
-        return;
-      }
-
-      if (message.length <= 5) {
-        alert('El mensaje debe tener al menos 5 caracteres en su cuerpo.');
-        return;
-      }
-
-      var mailTo = 'mailto:' + transmitter + '?subject=' + encodeURIComponent(name) + '&body=' + encodeURIComponent(message);
-      window.location.href = mailTo;
-      modalContact.style.display = 'none';
-
-      btnStart.disabled=false;
-    }
+  function showModalContact() {
+    modalContact.style.display = 'block';
+    btnStart.disabled=true;
   }
 
-  // Al hacer click al botón "Comenzar" inicia el juego
-  btnStart.addEventListener('click', function () {
-    startGame();
-  });
+  // Al hacer click en "Cancelar" en el modal de contacto se oculta
+  var btnCancel = document.getElementById('btnCancel');
+  btnCancel.addEventListener('click', hideModalContact);
 
-  function startGame() {
-    lvl++;
-    btnStart.textContent = 'Nivel ' + lvl;
-    var randomColor = colors[Math.floor(Math.random() * colors.length)];
-    sequence.push(randomColor);
-    btnStart.disabled = true;
+  function hideModalContact() {
+    modalContact.style.display = 'none';
+    btnStart.disabled=false;
+  }
 
-    showSequence();
+  // Al hacer click en "Enviar" del formulario de contacto valida y envia el correo
+  var btnSendEmail = document.getElementById('btnSend');
+  btnSendEmail.addEventListener('click', sendEmail);
+
+  function sendEmail() {
+    var txtTransmitter = document.getElementById('emailContact');
+    var transmitter = txtTransmitter.value;
+    var txtName = document.getElementById('nameContact');
+    var name = txtName.value;
+    var txtMessage = document.getElementById('txtMessage');
+    var message = txtMessage.value;
+
+    var validateName = /^[a-zA-Z0-9\s]+$/;
+    if (!validateName.test(name)) {
+      alert('El nombre debe ser alfanumérico.');
+      return;
+    }
+
+    var validateEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!validateEmail.test(transmitter)) {
+      alert('El email ingresado es inválido.');
+      return;
+    }
+
+    if (message.length <= 5) {
+      alert('El mensaje debe tener al menos 5 caracteres en su cuerpo.');
+      return;
+    }
+
+    var mailTo = 'mailto:' + transmitter + '?subject=' + encodeURIComponent(name) + '&body=' + encodeURIComponent(message);
+    window.location.href = mailTo;
+    modalContact.style.display = 'none';
+
+    btnStart.disabled=false;
+  }
+
+  // Función para mostrar el color en pantalla (efecto de iluminación)
+  function showColor(color) {
+    btnColors.forEach(function (square) {
+      var colorSquare = square.dataset.color;
+      if (colorSquare === color) {
+        square.style.backgroundColor = color;
+        setTimeout(function () {
+          square.style.backgroundColor = '';
+        }, 500);
+      }
+    });
   }
 
   // Función para mostrar la secuencia de colores al jugador
@@ -120,18 +120,48 @@ window.addEventListener('load', function () {
     userSequence = sequence.slice();
   }
 
-  // Función para mostrar el color en pantalla (efecto de iluminación)
-  function showColor(color) {
-    btnColors.forEach(function (square) {
-      var colorSquare = square.dataset.color;
-      if (colorSquare === color) {
-        square.style.backgroundColor = color;
-        setTimeout(function () {
-          square.style.backgroundColor = '';
-        }, 500);
-      }
-    });
+  // Funcion para comenzar el juego
+  function startGame() {
+    lvl++;
+    btnStart.textContent = 'Nivel ' + lvl;
+    var randomColor = colors[Math.floor(Math.random() * colors.length)];
+    sequence.push(randomColor);
+    btnStart.disabled = true;
+
+    showSequence();
   }
+
+  // Al hacer click al botón "Comenzar" inicia el juego
+  btnStart.addEventListener('click', function () {
+    startGame();
+  });
+
+  // Función para mostrar el modal de "Perdiste" con el puntaje obtenido
+  function showModalLose(lvlObtained, points) {
+    var modalLose = document.getElementById('modalLose');
+    var lvlLoseText = document.getElementById('levelLose');
+    var scoreLoseText = document.getElementById('scoreLose');
+    lvlLoseText.textContent = lvlObtained;
+    scoreLoseText.textContent = points;
+    modalLose.style.display = 'block';
+
+    // Cerrar el modal de "Perdiste"
+    var btnCloseModal = document.getElementById('btnCloseModal');
+    btnCloseModal.addEventListener('click', function () {
+      modalLose.style.display = 'none';
+      btnStart.disabled = false;
+    });
+
+    // Se reinicia la partida desde el modal de "Perdiste" al hacer click en el boton
+    var btnRestart = document.getElementById('btnRestart');
+    btnRestart.addEventListener('click', function () {
+      modalLose.style.display = 'none';
+      playerScore = 0;
+      lvl = 0;
+      sequence = [];
+      startGame();
+    });
+  };
 
   // Función para validar la selección del jugador y actualizar el puntaje
   function validateUserSelection(event) {
@@ -167,30 +197,4 @@ window.addEventListener('load', function () {
     square.addEventListener('click', validateUserSelection);
   });
 
-  // Función para mostrar el modal de "Perdiste" con el puntaje obtenido
-  function showModalLose(lvlObtained, points) {
-    var modalLose = document.getElementById('modalLose');
-    var lvlLoseText = document.getElementById('levelLose');
-    var scoreLoseText = document.getElementById('scoreLose');
-    lvlLoseText.textContent = lvlObtained;
-    scoreLoseText.textContent = points;
-    modalLose.style.display = 'block';
-
-    // Cierra el modal de "Perdiste"
-    var btnCloseModal = document.getElementById('btnCloseModal');
-    btnCloseModal.addEventListener('click', function () {
-      modalLose.style.display = 'none';
-      btnStart.disabled = false;
-    });
-
-    // Se reinicia la partida desde el modal de "Perdiste" al hacer click en el boton
-    var btnRestart = document.getElementById('btnRestart');
-    btnRestart.addEventListener('click', function () {
-      modalLose.style.display = 'none';
-      playerScore = 0;
-      lvl = 0;
-      sequence = [];
-      startGame();
-    });
-  }
 });
